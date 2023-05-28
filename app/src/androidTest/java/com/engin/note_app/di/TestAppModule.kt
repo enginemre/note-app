2bcf96e440d5729ef1826184b5eb39787b445b1a
enginemre <emre.engin@getir.com>
2023-05-28 22:48:28 +0300

    Added hilt support for testing

---
+++ app/src/androidTest/java/com/engin/note_app/di/TestAppModule.kt
@@ -0,0 +1,29 @@
+package com.engin.note_app.di
+
+import com.engin.note_app.data.local.NoteDatabase
+import com.engin.note_app.data.repository.FakeRepository
+import com.engin.note_app.data.repository.NoteRepositoryImpl
+import com.engin.note_app.domain.repository.NoteRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule{
+
+
+    @Provides
+    @Named("fake_repository")
+    @Singleton
+    fun provideNoteRepository(
+    ) : NoteRepository
+    {
+        return  FakeRepository()
+    }
+
+}
---
+++ app/src/debug/java/com/engin/note_app/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.engin.note_app
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity() {
+}
